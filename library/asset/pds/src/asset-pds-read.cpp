a866f25c1c57de1a3d888876c028db74e36269d6
aodq <0@aodq.net>
2022-03-31 17:40:46 -0700

    add bool type to pds

--- library/asset/pds/src/asset-pds-read.cpp
+++ library/asset/pds/src/asset-pds-read.cpp
@@ -291,6 +291,10 @@ PuleDsValue pdsParseValue(
     return { 0 };
   }
 
+  if (value == "true" || value = "false") {
+    return puleDsCreateBool(value == "true");
+  }
+
   // example, "" "asdf"
   if (value.size() > 2 && value[0] == '"' && value[value.size()-1] == '"') {
     value = value.substr(1, value.size()-1);
