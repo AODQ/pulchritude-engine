74dbc417e247a6b8a8aa5dd952920ea593abe800
aodq <0@aodq.net>
2022-04-06 20:38:36 -0700

    improve error logs

--- library/error/src/error.c
+++ library/error/src/error.c
@@ -10,7 +10,32 @@ PuleError puleError() {
 
 uint32_t puleErrorConsume(PuleError * const error) {
   if (error->id != 0) {
-    puleLog("ERROR [%zu]: %s", error->id, error->description.contents);
+    if (error->sourceLocationNullable) {
+      #define fileRelLength 6
+      char const * fileRel[fileRelLength] = {
+        error->sourceLocationNullable, error->sourceLocationNullable,
+        error->sourceLocationNullable, error->sourceLocationNullable,
+        error->sourceLocationNullable, error->sourceLocationNullable,
+      };
+      for (char const * fileIt = fileRel[0]; *fileIt != '\0'; ++ fileIt) {
+        if (*fileIt == '/' || *fileIt == '\\') {
+          for (int it = 0; it < fileRelLength-1; ++ it) {
+            fileRel[it] = fileRel[it+1];
+          }
+          fileRel[fileRelLength-1] = fileIt+1;
+        }
+      }
+      #undef fileRelLength
+      puleLogError(
+        "(%s:%zu [%zu]): %s",
+        fileRel[0],
+        error->lineNumber,
+        error->id,
+        error->description.contents
+      );
+    } else {
+      puleLogError("[%zu]: %s", error->id, error->description.contents);
+    }
     puleStringDeallocate(&error->description);
   }
   return error->id;
